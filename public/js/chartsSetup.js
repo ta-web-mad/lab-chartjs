2896e66957377c42bb219c7661b6bd3b03dfcaca
Helena Sánchez <helena.sangar@gmail.com>
2023-05-11 17:02:49 +0200

    done Carlos y Helena

--- public/js/chartsSetup.js
+++ public/js/chartsSetup.js
@@ -3,11 +3,155 @@ const colors = {
   alphas: ['rgba(116, 72, 194, 0.4)', 'rgba(33, 192, 215, 0.4)', 'rgba(217, 158, 43, 0.4)', 'rgba(205, 58, 129, 0.4)', 'rgba(156, 153, 204, 0.4)', 'rgba(225, 78, 202, 0.4)']
 }
 
-axios
-  .get('https://multiapi-app.fly.dev/coasters/allCoasters')
-  .then(({ data }) => printCoastersCharts(data))
-  .catch(err => console.log(err))
+const getApiData = () => {
 
-function printCoastersCharts(coasters) {
-  console.log('PARTY!', coasters)
+  axios
+    .get('https://multiapi-app.fly.dev/coasters/allCoasters')
+    .then(({ data }) => {
+      renderFirstChart(data)
+      renderSecondChart(data)
+      renderThirdChart(data)
+      renderFourthChart(data)
+      renderFifthChart(data)
+
+    })
+    .catch(err => console.log(err))
 }
+
+
+
+const renderFirstChart = coasters => {
+
+  const selectedCoasters = coasters.slice(0, 5)
+
+  const data = {
+    labels: selectedCoasters.map(elm => elm.name),
+    datasets: [{
+      data: selectedCoasters.map(elm => elm.speed),
+      backgroundColor: colors.alphas,
+      borderColor: colors.solids,
+      borderWidth: 2
+    }]
+  }
+
+  new Chart('chart1', { type: 'bar', data })
+}
+
+
+
+const renderSecondChart = coasters => {
+
+  const data = {
+    labels: ['< 1000m', '1000m - 1500m', '< 1500m'],
+    datasets: [{
+      data: [
+        coasters.filter(elm => elm.length < 1000).length,
+        coasters.filter(elm => elm.length > 1000 && elm.length < 1500).length,
+        coasters.filter(elm => elm.length > 1500).length,
+      ],
+      backgroundColor: colors.alphas,
+      borderColor: colors.solids,
+      borderWidth: 2
+    }]
+  }
+
+  const options = {
+    plugins: {
+      legend: {
+        display: true
+      }
+    }
+  }
+
+  new Chart('chart2', { type: 'doughnut', data, options })
+}
+
+
+
+const renderThirdChart = coasters => {
+
+  const data = {
+    labels: ['EEUU', 'España', 'Japón', 'China'],
+    datasets: [{
+      data: [
+        coasters.filter(elm => elm.country === 'United States').length,
+        coasters.filter(elm => elm.country === 'Spain').length,
+        coasters.filter(elm => elm.country === 'Japan').length,
+        coasters.filter(elm => elm.country === 'China').length,
+      ],
+      backgroundColor: colors.alphas,
+      borderColor: colors.solids,
+      borderWidth: 2
+    }]
+  }
+
+  const options = {
+    plugins: {
+      legend: {
+        display: true
+      }
+    }
+  }
+
+  new Chart('chart3', { type: 'polarArea', data, options })
+}
+
+
+
+const renderFourthChart = coasters => {
+
+  const inversionCoasters = coasters.filter(elm => elm.inversions > 5)
+
+  const data = {
+    labels: inversionCoasters.map(elm => elm.name),
+    datasets: [{
+      data: inversionCoasters.map(elm => elm.inversions),
+      backgroundColor: colors.alphas,
+      borderColor: colors.solids,
+      borderWidth: 2
+    }]
+  }
+
+  const options = {
+    plugins: {
+      legend: {
+        display: true
+      }
+    }
+  }
+
+  new Chart('chart4', { type: 'radar', data, options })
+}
+
+
+
+const renderFifthChart = coasters => {
+
+  const selectedCoasters = coasters.slice(0, 5)
+
+  const data = {
+    labels: selectedCoasters.map(elm => elm.name),
+    datasets: [
+      {
+        data: selectedCoasters.map(elm => elm.speed),
+        backgroundColor: colors.alphas,
+        borderColor: colors.solids,
+        borderWidth: 2
+      },
+      {
+        data: selectedCoasters.map(elm => elm.height),
+        backgroundColor: colors.alphas,
+        borderColor: colors.solids,
+        borderWidth: 2,
+        type: 'line'
+      }
+    ],
+  }
+
+
+  new Chart('chart5', { type: 'bar', data })
+
+}
+
+
+getApiData()
